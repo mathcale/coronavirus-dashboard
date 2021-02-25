d32cf2f86e837bef48a8f58407dc8e560e15d598
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-02-25 18:03:58 -0300

    fix(scripts): wrapping in async fn

--- scripts/syncBrazilData.js
+++ scripts/syncBrazilData.js
@@ -10,87 +10,89 @@ const client = new faunadb.Client({
 const apiEndpoint = process.env.COVID19_DASH_BRAZIL_ENDPOINT;
 const apiToken =  process.env.COVID19_DASH_BRAZILIO_TOKEN;
 
-try {
-  console.log('Fetching latest states\' data...');
+(async () => {
+  try {
+    console.log('Fetching latest states\' data...');
 
-  const responseBrazilLatestStates = await fetch(`${apiEndpoint}?is_last=True&place_type=state&format=json`, {
-    headers: {
-      Authorization: `Token ${apiToken}`,
-      'Accept': 'application/json',
-    }
-  });
+    const responseBrazilLatestStates = await fetch(`${apiEndpoint}?is_last=True&place_type=state&format=json`, {
+      headers: {
+        Authorization: `Token ${apiToken}`,
+        'Accept': 'application/json',
+      }
+    });
 
-  const brazilLatestStates = await responseBrazilLatestStates.json();
+    const brazilLatestStates = await responseBrazilLatestStates.json();
 
-  console.log('Waiting 2 secs...');
-  await wait(2000);
-
-  console.log('Fetching latest data from Brazil...');
-
-  const responseBrazilTodayCases = await fetch(`${apiEndpoint}?is_last=True&place_type=city&format=json`, {
-    headers: {
-      Authorization: `Token ${apiToken}`,
-      'Accept': 'application/json',
-    }
-  });
-
-  const brazilTodayCases = await responseBrazilTodayCases.json();
-
-  const totalPagesTodayCases = Math.ceil(brazilTodayCases.count / 1000);
-  const todayCases = [
-    ...brazilTodayCases.results,
-  ];
-
-  for (let page = 2; page <= totalPagesTodayCases; page++) {
     console.log('Waiting 2 secs...');
     await wait(2000);
 
-    console.log(`Fetching page ${page}...`);
-    const responseBrazilTodayCasesNext = await fetch(`${apiEndpoint}?is_last=True&place_type=city&&page=${page}&format=json`);
-    const brazilTodayCasesNext = await responseBrazilTodayCasesNext.json();
+    console.log('Fetching latest data from Brazil...');
 
-    todayCases.push(brazilTodayCasesNext.results);
+    const responseBrazilTodayCases = await fetch(`${apiEndpoint}?is_last=True&place_type=city&format=json`, {
+      headers: {
+        Authorization: `Token ${apiToken}`,
+        'Accept': 'application/json',
+      }
+    });
+
+    const brazilTodayCases = await responseBrazilTodayCases.json();
+
+    const totalPagesTodayCases = Math.ceil(brazilTodayCases.count / 1000);
+    const todayCases = [
+      ...brazilTodayCases.results,
+    ];
+
+    for (let page = 2; page <= totalPagesTodayCases; page++) {
+      console.log('Waiting 2 secs...');
+      await wait(2000);
+
+      console.log(`Fetching page ${page}...`);
+      const responseBrazilTodayCasesNext = await fetch(`${apiEndpoint}?is_last=True&place_type=city&&page=${page}&format=json`);
+      const brazilTodayCasesNext = await responseBrazilTodayCasesNext.json();
+
+      todayCases.push(brazilTodayCasesNext.results);
+    }
+
+    let biggestConfirmedFound = 0;
+    let mostAffectedState = '';
+
+    const summary = brazilLatestStates.results.reduce(
+      (acc, curr) => {
+        if (curr.confirmed > biggestConfirmedFound) {
+          biggestConfirmedFound = curr.confirmed;
+          mostAffectedState = curr.state;
+        } else {
+          mostAffectedState = acc.mostAffectedState;
+        }
+
+        return {
+          confirmed: acc.confirmed + curr.confirmed,
+          deaths: acc.deaths + curr.deaths,
+          mostAffectedState,
+        }
+      },
+      { confirmed: 0, deaths: 0, mostAffectedState: '' },
+    );
+
+    const data = {
+      summary,
+      todayCases,
+      states: brazilLatestStates.results,
+    };
+
+    console.log('Saving data on database...');
+
+    const result = await client.query(
+      q.Create(
+        q.Collection('summaries'),
+        { data },
+      )
+    );
+
+    this.res.status(201).json({ result });
+  } catch (err) {
+    this.res.status(500).json({
+      message: err.message,
+    });
   }
-
-  let biggestConfirmedFound = 0;
-  let mostAffectedState = '';
-
-  const summary = brazilLatestStates.results.reduce(
-    (acc, curr) => {
-      if (curr.confirmed > biggestConfirmedFound) {
-        biggestConfirmedFound = curr.confirmed;
-        mostAffectedState = curr.state;
-      } else {
-        mostAffectedState = acc.mostAffectedState;
-      }
-
-      return {
-        confirmed: acc.confirmed + curr.confirmed,
-        deaths: acc.deaths + curr.deaths,
-        mostAffectedState,
-      }
-    },
-    { confirmed: 0, deaths: 0, mostAffectedState: '' },
-  );
-
-  const data = {
-    summary,
-    todayCases,
-    states: brazilLatestStates.results,
-  };
-
-  console.log('Saving data on database...');
-
-  const result = await client.query(
-    q.Create(
-      q.Collection('summaries'),
-      { data },
-    )
-  );
-
-  this.res.status(201).json({ result });
-} catch (err) {
-  this.res.status(500).json({
-    message: err.message,
-  });
-}
+})();
