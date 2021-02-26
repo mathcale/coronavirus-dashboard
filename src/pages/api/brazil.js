f8afee301806bee7cf96dc48d8e1a145013ea4bd
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-02-26 14:55:54 -0300

    feat(api/brazil): querying data from our database

--- src/pages/api/brazil.js
+++ src/pages/api/brazil.js
@@ -1,4 +1,9 @@
-import fetch from 'isomorphic-unfetch';
+import faunadb from 'faunadb';
+
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.COVID19_DASH_FAUNADB_SECRET,
+});
 
 export default async (req, res) => {
   if (req.method !== 'GET') {
@@ -9,52 +14,12 @@ export default async (req, res) => {
     res.end(JSON.stringify({ message: 'Method Not Allowed!' }));
   }
 
-  const endpoint = process.env.COVID19_DASH_BRAZIL_ENDPOINT;
-
-  const responseBrazilLatestStates = await fetch(`${endpoint}?is_last=True&place_type=state&format=json`);
-  const brazilLatestStates = await responseBrazilLatestStates.json();
-
-  const responseBrazilTodayCases = await fetch(`${endpoint}?is_last=True&place_type=city&format=json`);
-  const brazilTodayCases = await responseBrazilTodayCases.json();
-
-  const totalPagesTodayCases = Math.ceil(brazilTodayCases.count / 1000);
-  const todayCases = [
-    ...brazilTodayCases.results,
-  ];
-
-  for (let page = 2; page <= totalPagesTodayCases; page++) {
-    const responseBrazilTodayCasesNext = await fetch(`${endpoint}?is_last=True&place_type=city&&page=${page}&format=json`);
-    const brazilTodayCasesNext = await responseBrazilTodayCasesNext.json();
-
-    todayCases.push(brazilTodayCasesNext.results);
-  }
-
-  let biggestConfirmedFound = 0;
-  let mostAffectedState = '';
-
-  const summary = brazilLatestStates.results.reduce(
-    (acc, curr) => {
-      if (curr.confirmed > biggestConfirmedFound) {
-        biggestConfirmedFound = curr.confirmed;
-        mostAffectedState = curr.state;
-      } else {
-        mostAffectedState = acc.mostAffectedState;
-      }
-
-      return {
-        confirmed: acc.confirmed + curr.confirmed,
-        deaths: acc.deaths + curr.deaths,
-        mostAffectedState,
-      }
-    },
-    { confirmed: 0, deaths: 0, mostAffectedState: '' },
+  const result = await client.query(
+    q.Get(
+      q.Ref(q.Collection('summaries'),
+      process.env.COVID19_DASH_FAUNADB_DOCUMENT_REF_ID),
+    )
   );
 
-  const data = {
-    summary,
-    todayCases,
-    states: brazilLatestStates.results,
-  };
-
-  res.status(200).json(JSON.stringify(data));
+  res.status(200).json(JSON.stringify(result.data));
 };
